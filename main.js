05ecf9f3ea4a031db5f2f7bb13542f1c686a3a60
AbstractZion <86538881+AbstractZion@users.noreply.github.com>
2022-01-05 18:47:41 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML=" ";
+    recognition.start();
+}
+
+recognition.onresut=function(event){
+    console.log(event);
+    var Content=event.result[0][0].transcript;
+    console.log(Content);
+    document.getElementById("textbox").innerHTML=Content;
+}
